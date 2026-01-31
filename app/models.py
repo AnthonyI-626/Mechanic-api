cce1209467d2afc357977b0ba36132b3d4b56a7d
Anthony Irizarry <tonyirizarry69@gmail.com>
2026-01-30 21:27:37 -0700

    Add tickets relationship to Customers model

--- app/models.py
+++ app/models.py
@@ -26,6 +26,8 @@ class Customers(Base):
     password: Mapped[str] = mapped_column(db.String(255), nullable=False)
     phone: Mapped[str] = mapped_column(db.String(15), unique=True, nullable=False)
     DOB: Mapped[str] = mapped_column(db.String(20), nullable=True)
+    
+    tickets = relationship("ServiceTicket", back_populates="customer")
 
     
 ticket_parts = db.Table( 'ticket_parts', db.Column(
@@ -36,14 +38,22 @@ ticket_parts = db.Table( 'ticket_parts', db.Column(
 class ServiceTicket(Base):
     __tablename__ = 'service_tickets'
     
-    id: Mapped[int] = mapped_column(primary_key = True)
-    description: Mapped[str] = mapped_column(db.String(500), nullable = False)
-    date_created: Mapped[date] = mapped_column(db.Date)
-    status: Mapped[str] = mapped_column(db.String(50), nullable = False) 
-    
-    mechanic_id: Mapped[int] = mapped_column(ForeignKey('mechanics.id'), nullable = True)
-    mechanic: Mapped['Mechanic'] = relationship(Mechanic, back_populates='tickets')
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    customer_id: Mapped[int] = mapped_column(
+        db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    mechanic_id: Mapped[int] = mapped_column(
+        db.Integer, db.ForeignKey('mechanics.id'), nullable=False)
+
+    description: Mapped[str] = mapped_column(db.String(500), nullable=False)
+    status: Mapped[str] = mapped_column(db.String(50), nullable=False)
+    date_created: Mapped[date] = mapped_column(db.Date, default=date.today)
+
+    customer = relationship("Customers", back_populates="tickets")
+    mechanic = relationship("Mechanic", back_populates="tickets")
+
     parts = db.relationship('Inventory', secondary='ticket_parts', back_populates='tickets')
+
      
     
 class Inventory(Base):
