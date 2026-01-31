8c2908a6d9d5bf5d5369d431b7979242e8ffdd7a
Anthony Irizarry <tonyirizarry69@gmail.com>
2026-01-30 22:06:13 -0700

    Add nested mechanic and parts fields to ServiceTicket schema

--- app/blueprints/ServiceTicket/schemas.py
+++ app/blueprints/ServiceTicket/schemas.py
@@ -1,10 +1,28 @@
+from app.models import ServiceTicket, Mechanic, Inventory
 from app.extensions import ma
-from app.models import ServiceTicket
+
+
+class MechanicNestedSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Mechanic
+        fields = ("id", "name", "email", "phone")
+
+
+class InventoryNestedSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Inventory
+        fields = ("id", "name", "price")
+
 
 class ServiceTicketSchema(ma.SQLAlchemyAutoSchema):
+    mechanic = ma.Nested(MechanicNestedSchema)
+    parts = ma.List(ma.Nested(InventoryNestedSchema))
+
     class Meta:
         model = ServiceTicket
         include_fk = True
-        
+        load_instance = True
+
+
 service_ticket_schema = ServiceTicketSchema()
 service_tickets_schema = ServiceTicketSchema(many=True)
