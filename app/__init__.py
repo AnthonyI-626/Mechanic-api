36b1aed0031fa8b772fe74a3a894661612826b3b
Anthony Irizarry <tonyirizarry69@gmail.com>
2026-01-30 21:49:17 -0700

    Changed config_name value

--- app/__init__.py
+++ app/__init__.py
@@ -22,8 +22,8 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 def create_app(config_name=None):
     app = Flask(__name__)
     
-    if config_name is None:
-        config_name = 'DevelopmentConfig'
+    if config_name is None: 
+      config_name = 'TestingConfig'
         
     app.config.from_object(f'config.{config_name}')
     
